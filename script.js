69b96f0b09d1a6a020c686a4e4b26acd0f2035d6
Crisppu <cristobalyat69@hotmail.com>
2022-11-06 19:39:55 -0600

    funcion de agg tarea

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ import deleteIcon from "./components/deleteIcon.js";
 (() => {
 
   const btn = document.querySelector("[data-form-btn]");
+  const addTask  = (evento) =>{
+    const list = document.querySelector("[data-list]")// estamos ubicados en la etiqueta ul
+    const task = createTask(evento);
+    list.appendChild(task);
+  }
   const createTask = (evento) => {
     evento.preventDefault(); //evita que recargue la pagina
     const input = document.querySelector("[data-form-input]");
@@ -12,7 +17,7 @@ import deleteIcon from "./components/deleteIcon.js";
     const date = calendario.value;
     const dateFormat = moment(date).format("DD/MM/YYYY");
     console.log(dateFormat)
-    const list = document.querySelector("[data-list]")// estamos ubicados en la etiqueta ul
+    //const list = document.querySelector("[data-list]")// estamos ubicados en la etiqueta ul
     const task = document.createElement('li'); //hacemos que se cree un elemento li en html
     task.classList.add("card"); //agregamos los estilos del css class="card"
     input.value = ""; // nos limpia el input
@@ -30,10 +35,10 @@ import deleteIcon from "./components/deleteIcon.js";
     task.appendChild(taskContent);
     task.appendChild(dateElement)
     task.appendChild(deleteIcon());
-    list.appendChild(task);
-    //console.log(contenido)
+    //list.appendChild(task);
+    return task;
   };
 
-  btn.addEventListener("click", createTask);
+  btn.addEventListener("click", addTask);
 
 })() //immediately invoked function expression IIFE
