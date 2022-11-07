38b1f4c511cfe0b9b1544362e0575744833b076a
Crisppu <cristobalyat69@hotmail.com>
2022-11-07 17:35:00 -0600

    config del localStorage funcional

--- components/addTask.js
+++ components/addTask.js
@@ -6,15 +6,17 @@ export const addTask = evento => {
   const task = createTask(evento);
   list.appendChild(task);
 };
-const taskList = []; //creamos este listado para guardar informacion en el navegador
+//const taskList = []; //creamos este listado para guardar informacion en el navegador
 const createTask = evento => {
   evento.preventDefault(); //evita que recargue la pagina
+  const taskList = JSON.parse(localStorage.getItem("tareas")) || []; // traemos el listado de string que esta almacenado desde el navegador pero no podemos agg en el array sin antes convertirlo con JSON.parse()
+  console.log(taskList)
   const input = document.querySelector("[data-form-input]");
   const calendario = document.querySelector("[data-form-date]");
   const value = input.value; //obtiene el contenido dentro del intup
   const date = calendario.value;
   const dateFormat = moment(date).format("DD/MM/YYYY");
-  console.log(dateFormat);
+  //console.log(dateFormat);
   //const list = document.querySelector("[data-list]")// estamos ubicados en la etiqueta ul
   const task = document.createElement("li"); //hacemos que se cree un elemento li en html
   task.classList.add("card"); //agregamos los estilos del css class="card"
@@ -27,7 +29,7 @@ const createTask = evento => {
   };
   taskList.push(taskObj);
   //sessionStorage.setItem('tasks',JSON.stringify(taskObj))
-  localStorage.setItem("tareas", JSON.stringify(taskList));
+  localStorage.setItem("tareas", JSON.stringify(taskList)); //convertimos de objeto a string para que se almacene desde el navegador "application" - JSON.stringify
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
@@ -36,7 +38,7 @@ const createTask = evento => {
   //task.innerHTML = content
   const dateElement = document.createElement("span");
   dateElement.innerHTML = dateFormat;
-  console.log(dateElement);
+  //console.log(dateElement);
   task.appendChild(taskContent);
   task.appendChild(dateElement);
   task.appendChild(deleteIcon());
