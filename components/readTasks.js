e6bcfc49f95aef159d4cd0d36ba9d3c1940bd515
Crisppu <cristobalyat69@hotmail.com>
2022-11-09 11:21:18 -0600

    ordenamos las fechas de menor a mayorDeLosCursos

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,12 +1,13 @@
-import { uniqueDates } from "../services/date.js";
+import { uniqueDates, orderDates } from "../services/date.js";
 import { createTask } from "./addTask.js";
 import dateElement from "./dateElement.js";
 export const readTasks = () => {// se le cambio de nombre readTasks a displayTasks
     const list = document.querySelector("[data-list]");
     const tasksList = JSON.parse(localStorage.getItem("tareas")) || [];
     const dates = uniqueDates(tasksList);
+    orderDates(dates)
+
     dates.forEach((date) => {
-        //console.log(date);
         const dateMoment = moment(date, 'DD/MM/YYYY');
         list.appendChild(dateElement(date));
         tasksList.forEach((task) => {
--- services/date.js
+++ services/date.js
@@ -9,4 +9,13 @@ export const uniqueDates = (tasks) =>{
     });
  return unique;
 
-}
+};
+
+export const orderDates = (dates) =>{
+return dates.sort((a, b) =>{
+  const firsDate = moment(a, "DD/MM/YYYY");
+  const secondDate = moment(b, "DD/MM/YYYY");
+  return firsDate - secondDate;
+
+});
+};
